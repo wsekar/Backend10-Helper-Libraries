66c529e5bb96fb0705bf1ebb2f37a3486f40a003
Wibiati Sekar K <89697985+wsekar@users.noreply.github.com>
2021-11-18 21:54:15 +0700

    Update Page.php

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -6,15 +6,15 @@ class Page extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->library('cart');
-		$this->load->model('M_cart');
+		$this->load->library('cart'); //load library cart
+		$this->load->model('M_cart'); //load model M_cart
 	}
 	public function index()
 	{
-		$data['produk'] = $this->M_cart->get_produk_all();
-		$data['kategori'] = $this->M_cart->get_kategori_all();
-		$this->load->view('themes/header', $data);
-		$this->load->view('shopping/list_produk', $data);
-		$this->load->view('themes/footer');
+		$data['produk'] = $this->M_cart->get_produk_all(); //get all produk dari M_cart dan disimpan dalam variabel produk
+		$data['kategori'] = $this->M_cart->get_kategori_all();//get all kategori dari M_cart dan disimpan dalam variabel kategori
+		$this->load->view('themes/header', $data); //load view themes/header
+		$this->load->view('shopping/list_produk', $data); //load view shopping/list_produk, dan menampilkan data dari produk dan kategori
+		$this->load->view('themes/footer');//load view themes/footer
 	}
 }
